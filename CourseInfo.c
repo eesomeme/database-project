cd06af819a25988c9781164f7cc6348c31b4f3a9
maria weber <mweber13@u.rochester.edu>
2016-11-26 17:30:29 -0500

    add initial files

---
+++ CourseInfo.c
@@ -0,0 +1,11 @@
+
+/**
+ * ex: < CS173, 123456789, B- >
+ */
+struct CSG
+{
+	char CourseID[5];
+	int StudentID;
+	char Grade[2]; 
+	
+};
---
+++ LocationTable.c
@@ -0,0 +1,9 @@
+
+/**
+ * ex: < CS173, LAT201 >
+ */
+struct Location
+{
+	char CourseID[5];
+	char Room[6]; 
+};
